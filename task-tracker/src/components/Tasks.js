869685c480669559f8cf8502a8908eff2b8b54a6
Remek-P <remek.piotrowski@wp.pl>
2023-02-24 13:12:07 +0100

    Tasks.js - if no tasks show message

--- task-tracker/src/components/Tasks.js
+++ task-tracker/src/components/Tasks.js
@@ -4,6 +4,8 @@ import { Task } from "./Task";
 
 export function Tasks ({ tasks, setTasks }) {
 
+    const noTasksMessage = "You are all set!"
+
     const deleteTask = (id) => {
         setTasks(tasks.filter(task => task.id !== id))
         console.log("click", id)
@@ -12,12 +14,14 @@ export function Tasks ({ tasks, setTasks }) {
     return (
         <>
             {
-                tasks.map((task =>
-                    <Task
-                        key={task.id}
-                        task={task}
-                        deleteTask={deleteTask}
-                    />))
+                tasks.length !== 0
+                    ? tasks.map((task =>
+                        <Task
+                            key={task.id}
+                            task={task}
+                            deleteTask={deleteTask}
+                        />))
+                    : noTasksMessage
             }
         </>
     )
